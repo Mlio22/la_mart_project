33bf7259f9b4a7cc58687967fac9403b56281fd8
muhammat lio pratama <muhammatliopratama@gmail.com>
2021-08-26 23:25:11 +0700

    feat: added database checking before migrating

--- db/migrator.js
+++ db/migrator.js
@@ -2,13 +2,21 @@
 const config = require("../config/config_sequelize.json");
 const { Sequelize } = require("sequelize");
 const { Umzug } = require("umzug");
+const mysql = require("mysql2");
 
 const { host, user, port, password, database } = config.database;
+
+// check if database exists or create one
+const connection = mysql.createConnection({ host, port, user, password });
+connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+// init sequelize
 const sequelize = new Sequelize(database, user, password, {
   host: host,
   dialect: "mysql",
 });
 
+// init umzug
 const umzug = new Umzug({
   migrations: {
     glob: ["migrations/*.js", { cwd: __dirname }],
