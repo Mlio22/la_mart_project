3762f822ea22f373a6348547135b62d3c70bc773
muhammat lio pratama <muhammatliopratama@gmail.com>
2021-08-26 19:30:12 +0700

    organizing models

--- db/models/log/alasan_pengeluaran.js
+++ db/models/log/alasan_pengeluaran.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class alasan_pengeluaran extends Model {
     /**
@@ -12,12 +10,15 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  alasan_pengeluaran.init({
-    deskripsi_alasan_pengeluaran: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'alasan_pengeluaran',
-  });
+  }
+  alasan_pengeluaran.init(
+    {
+      deskripsi_alasan_pengeluaran: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "alasan_pengeluaran",
+    }
+  );
   return alasan_pengeluaran;
 };
--- db/models/stock/pemasukan_barang.js
+++ db/models/stock/pemasukan_barang.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class pemasukan_barang extends Model {
     /**
@@ -12,13 +10,16 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  pemasukan_barang.init({
-    id_stok_barang: DataTypes.INTEGER,
-    jumlah: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'pemasukan_barang',
-  });
+  }
+  pemasukan_barang.init(
+    {
+      id_stok_barang: DataTypes.INTEGER,
+      jumlah: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "pemasukan_barang",
+    }
+  );
   return pemasukan_barang;
 };
