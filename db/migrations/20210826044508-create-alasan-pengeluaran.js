17f73d2175e45498ddcda534876816cd675183f3
muhammat lio pratama <muhammatliopratama@gmail.com>
2021-08-27 11:19:29 +0700

    fix: removed unnececssary createdAt attribute

--- db/migrations/20210826044508-create-alasan-pengeluaran.js
+++ db/migrations/20210826044508-create-alasan-pengeluaran.js
@@ -13,10 +13,6 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
     });
   },
   down: async (queryInterface, Sequelize) => {
