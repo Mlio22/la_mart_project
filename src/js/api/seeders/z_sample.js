8de9a6256ec66a679910e9276e47a974e999239f
muhammat lio pratama <muhammatliopratama@gmail.com>
2021-09-07 11:46:11 +0700

    separate api and etc from pages

--- src/js/api/seeders/z_sample.js
+++ src/js/api/seeders/z_sample.js
@@ -1,14 +1,5 @@
 "use strict";
 
-module.exports = async (db) => {
-  await db.TransaksiKeseluruhan.create({
-    total_harus_dibayar: 2000,
-    dibayar_oleh_konsumen: 2000,
-    kembalian: 0,
-    total_keuntungan: 100,
-  });
-};
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
@@ -18,12 +9,6 @@ module.exports = {
           dibayar_oleh_konsumen: 2000,
           kembalian: 0,
           total_keuntungan: 100,
-        },
-      ]),
-      queryInterface.bulkInsert("laporan_harian", [
-        {
-          total_pendapatan: 2000000,
-          total_keuntungan: 200000,
         },
       ]),
     ]);
